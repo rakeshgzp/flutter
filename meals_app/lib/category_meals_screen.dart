ffc6ab5634e9ef81dca422a5a6ab96b97015a30c
rakeshgzp <rprakeshprasad00@gmail.com>
2020-08-25 21:54:01 +0530

    meals app routes and dummy_data added

---
+++ meals_app/lib/category_meals_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    // final CategoryId = routeArgs['id'];
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: ListView.builder(itemBuilder: (ctx, index) {}));
+  }
+}
