af90d6865be869ccd4ed039feab3fea2e394a8ab
rakeshgzp <rprakeshprasad00@gmail.com>
2020-08-26 21:44:06 +0530

    meal details page and route added

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import './category_meals_screen.dart';
-import './categories_screen.dart';
+import 'package:meals_app/screens/meal_detail_screen.dart';
+import 'screens/category_meals_screen.dart';
+import 'screens/categories_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,10 +31,11 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.bold,
             )),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
     );
   }
